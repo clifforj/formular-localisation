77a0f278b9cd32dee2605a31aebccab7cd161d40
John Clifford <clifforj@gmail.com>
2014-06-08 21:45:27 +0100

    Load resources from configured URLs

--- app/js/localisation.js
+++ app/js/localisation.js
@@ -4,24 +4,37 @@ angular.module('formularLocalisation', [ ])
     .value("formularLocalisationDefaults", {
         locales: [
             {
-                locale: "en-gb"
+                locale: "en-gb",
+                url: "localisation/en-gb.json"
             }
         ]
     })
     .service("formularLocalisationService",
-    ["$filter","$log", "$injector", "formularLocalisationDefaults",
-        function ($filter, $log, $injector, formularLocalisationDefaults) {
-            var config, locale;
+    ["$http","$filter","$log", "$injector", "formularLocalisationDefaults",
+        function ($http, $filter, $log, $injector, formularLocalisationDefaults) {
+            this.resourceAvailable = false
+            this.resources = [];
 
             this.setLocale = function (locale) {
-                if(this.isLocaleAvailable(locale))
+                if(this.isLocaleAvailable(locale)) {
                     this.locale = locale;
-                else
+                } else {
                     this.locale = this.config.locales[0].locale;
+                }
+
+                this.resourceAvailable = this.isResourceAvailable(this.locale);
             };
 
             this.isLocaleAvailable = function (locale) {
                 return $filter('filter')(this.config.locales, {locale:locale}) === 1;
+            };
+
+            this.getLocaleConfig = function (locale) {
+                return $filter('filter')(this.config.locales, {locale:locale})[0];
+            };
+
+            this.isResourceAvailable = function (locale) {
+                return this.resources[locale];
             }
 
             // Check for a user specified config, otherwise use the defaults
@@ -36,6 +49,23 @@ angular.module('formularLocalisation', [ ])
 
                 this.setLocale(this.config.locales[0].locale);
             };
+
+            this.loadCurrentLocalisationResource = function () {
+                if(this.resources[this.locale] == undefined) {
+                    $http({ method:"GET", url:this.getLocaleConfig(this.locale).url, cache:false })
+                        .success(angular.bind(this, this.setCurrentLocalisationResource))
+                        .error(function () {
+                            $log.log("formular: Failed to load resouce : " + this.config.locales[this.locale].url)
+                        });
+                }
+            }
+
+            this.setCurrentLocalisationResource = function (data) {
+                if(data != undefined) {
+                    this.resources[this.locale] = data;
+                    this.resourceAvailable = true;
+                }
+            }
 
             this.initConfig();
         }]);
--- test/unit/localisationTests.js
+++ test/unit/localisationTests.js
@@ -1,40 +1,74 @@
 'use strict';
 
 describe('formularLocalisation service', function () {
-    beforeEach(module('formularLocalisation'));
+    var flService, flDefaults, $httpBackend;
+    var enGbJson = [
+        {
+            key: "application.title",
+            value: "My Application"
+        },
+        {
+            key: "application.title",
+                value: "My Application"
+        }
+    ];
 
     describe('Configuration', function () {
-        it('will create a default config', inject(function (formularLocalisationDefaults) {
-            expect(formularLocalisationDefaults.locales.length).toEqual(1);
+        beforeEach(module('formularLocalisation'));
+        beforeEach(inject(function (formularLocalisationService, formularLocalisationDefaults) {
+            flService = formularLocalisationService;
+            flDefaults = formularLocalisationDefaults;
         }));
 
-        it('should load a default config if no user specified one exits',
-            inject(function (formularLocalisationService, formularLocalisationDefaults) {
-                expect(formularLocalisationService.config).toEqual(formularLocalisationDefaults);
-            }));
+        it('will create a default config', function () {
+            expect(flDefaults.locales.length).toEqual(1);
+        });
 
-        it('should set a default locale of "en-gb"', inject(function (formularLocalisationService) {
-            expect(formularLocalisationService.locale).toEqual('en-gb');
-        }));
+        it('should load a default config if no user specified one exits', function () {
+            expect(flService.config).toEqual(flDefaults);
+        });
 
-        it('should reset to default locale if an invalid/unavailable one is request', inject(function (formularLocalisationService) {
-            formularLocalisationService.setLocale("invalid");
-            expect(formularLocalisationService.locale).toEqual('en-gb');
-        }));
+        it('should set a default locale of "en-gb"', function () {
+            expect(flService.locale).toEqual('en-gb');
+        });
 
-        describe('User Specified', function () {
-            beforeEach(module('formularLocalisation', function ($provide) {
-                $provide.value('formularLocalisationConfig', {
-                    locales: [{
+        it('should reset to default locale if an invalid/unavailable one is request', function () {
+            flService.setLocale("invalid");
+            expect(flService.locale).toEqual('en-gb');
+        });
+    });
+
+    describe('Configuration - User Specified', function () {
+        beforeEach(module('formularLocalisation', function ($provide) {
+            $provide.value('formularLocalisationConfig', {
+                locales: [
+                    {
                         locale: "TEST"
-                    }]
-                });
-            }));
+                    }
+                ]
+            });
+        }));
 
-            it('should pick up a user specified config if available', inject(function (formularLocalisationService) {
-                expect(formularLocalisationService.config).not.toBeNull();
-                expect(formularLocalisationService.locale).toEqual('TEST');
-            }));
+        it('should pick up a user specified config if available', inject(function (formularLocalisationService) {
+            expect(formularLocalisationService.config).not.toBeNull();
+            expect(formularLocalisationService.locale).toEqual('TEST');
+        }));
+    });
+
+    describe('Resources', function () {
+        beforeEach(module('formularLocalisation'));
+        beforeEach(inject(function (_$httpBackend_, formularLocalisationService) {
+            flService = formularLocalisationService
+            $httpBackend = _$httpBackend_;
+            $httpBackend.expectGET('localisation/en-gb.json').
+                respond(enGbJson);
+        }));
+
+        it('should load the specified current locales resources', function () {
+            expect(flService.resources).toEqual([]);
+            flService.loadCurrentLocalisationResource();
+            $httpBackend.flush();
+            expect(flService.resources[flService.locale]).toBeDefined();
         });
     });
 });
